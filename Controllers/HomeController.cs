6bf5c92e3fde1eb00e2ed795ecbfd3786139d3c3
Sokolov <sokolov_ea@farmsib.ru>
2020-05-24 20:17:10 +0300

    q

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,11 +19,5 @@ namespace DocsRd.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public Object Rd()
-        {
-            Guid.TryParse(Request.Form["sessionId"], out Guid sessionId);
-            return sessionId.ToString();
-        }
     }
 }
---
+++ Controllers/RdController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DocsRd.Controllers
+{
+    public class RdController : Controller
+    {
+        public Object Index()
+        {
+            Object result = "DocsRd.Controllers.HomeController.Rd()<br />";
+            try
+            {
+                Guid.TryParse(Request.Form["sessionId"], out Guid sessionId);
+                result = PartialView(sessionId);
+            }
+            catch (Exception e) { result += e.ToString(); }
+            return result;
+        }
+    }
+}
